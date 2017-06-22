8ba9b7ea22570a6a091be503927f586f59648e48
Emily <emilini@gmail.com>
2017-06-21 20:41:36 -0500

    add nav bar with links to genres, artists, and songs.

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   resources :artists
   resources :genres
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  root 'genres#index'
 end
